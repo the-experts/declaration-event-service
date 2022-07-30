28f15c5707103e41a5138ce3ff050039bbe18a3f
Patrick Dronk <patrickdronk@me.com>
2022-07-30 09:34:33 +0200

    revert back to records

--- src/main/java/nl/codecentric/declarationeventservice/domain/user/UpdatePasswordCommand.java
+++ src/main/java/nl/codecentric/declarationeventservice/domain/user/UpdatePasswordCommand.java
@@ -5,45 +5,5 @@ import org.axonframework.modelling.command.TargetAggregateIdentifier;
 import java.util.Objects;
 import java.util.UUID;
 
-public final class UpdatePasswordCommand {
-    @TargetAggregateIdentifier
-    private final UUID employeeId;
-    private final String password;
-
-    public UpdatePasswordCommand(
-            UUID employeeId, String password) {
-        this.employeeId = employeeId;
-        this.password = password;
-    }
-
-    @TargetAggregateIdentifier
-    public UUID employeeId() {
-        return employeeId;
-    }
-
-    public String password() {
-        return password;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (UpdatePasswordCommand) obj;
-        return Objects.equals(this.employeeId, that.employeeId) &&
-                Objects.equals(this.password, that.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(employeeId, password);
-    }
-
-    @Override
-    public String toString() {
-        return "UpdatePasswordCommand[" +
-                "employeeId=" + employeeId + ", " +
-                "password=" + password + ']';
-    }
-
+public record UpdatePasswordCommand(@TargetAggregateIdentifier UUID employeeId, String password) {
 }
--- src/main/java/nl/codecentric/declarationeventservice/domain/user/UserCreatedEvent.java
+++ src/main/java/nl/codecentric/declarationeventservice/domain/user/UserCreatedEvent.java
@@ -3,58 +3,5 @@ package nl.codecentric.declarationeventservice.domain.user;
 import java.util.Objects;
 import java.util.UUID;
 
-final class UserCreatedEvent {
-    private final UUID id;
-    private final String name;
-    private final String email;
-    private final String password;
-
-    UserCreatedEvent(UUID id, String name, String email, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public UUID id() {
-        return id;
-    }
-
-    public String name() {
-        return name;
-    }
-
-    public String email() {
-        return email;
-    }
-
-    public String password() {
-        return password;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (UserCreatedEvent) obj;
-        return Objects.equals(this.id, that.id) &&
-                Objects.equals(this.name, that.name) &&
-                Objects.equals(this.email, that.email) &&
-                Objects.equals(this.password, that.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email, password);
-    }
-
-    @Override
-    public String toString() {
-        return "UserCreatedEvent[" +
-                "id=" + id + ", " +
-                "name=" + name + ", " +
-                "email=" + email + ", " +
-                "password=" + password + ']';
-    }
-
+record UserCreatedEvent(UUID id, String name, String email, String password) {
 }
