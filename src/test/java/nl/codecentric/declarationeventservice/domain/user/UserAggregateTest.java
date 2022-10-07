fa3a06c102f3619562e41ec9e967f0630c6e9a23
lars <chaoskie.lars+git@gmail.com>
2022-10-07 14:17:13 +0200

    adding a test that fails to see the pipeline results

--- src/test/java/nl/codecentric/declarationeventservice/domain/user/UserAggregateTest.java
+++ src/test/java/nl/codecentric/declarationeventservice/domain/user/UserAggregateTest.java
@@ -24,4 +24,12 @@ class UserAggregateTest {
                 .when(new CreateUserCommand(uuid, "John", "John@codecentric.nl", "password"))
                 .expectEvents(new UserCreatedEvent(uuid, "John", "John@codecentric.nl", "password"));
     }
+
+    @Test
+    void shouldFailOnPurposeTest() {
+        var uuid = UUID.randomUUID().toString();
+        fixture.givenNoPriorActivity()
+                .when(new CreateUserCommand(uuid, "John", "John@codecentric.nl", "password"))
+                .expectEvents(new UserCreatedEvent(uuid, "Bert", "Bert@codecentric.nl", "pwd"));
+    }
 }
